04754fbd979820846d220e7a9499b5a6d09f7dbf
DONDAJIN <hkazu.2@icloud.com>
2022-08-06 19:24:32 +0900

    modified

--- exp/exp001/exp001.py
+++ exp/exp001/exp001.py
@@ -1,8 +1,5 @@
-from msilib.schema import FileSFPCatalog
 import os
 EXP=''
 DIR='content/drive/MyDrive/Kaggle'
 INPUT=os.path.join(DIR,'Input')
 OUTPUT=os.path.join(DIR,INPUT)
-FileSFPCatalog
-fifjf
